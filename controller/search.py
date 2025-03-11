1dbf6c1f35e2086051cfe4778f202310fde98c24
Cole Robertson <colerobertson55@gmail.com>
2025-03-11 15:53:41 -0700

    Improved code efficiency.

--- controller/search.py
+++ controller/search.py
@@ -19,99 +19,41 @@ class SearchController:
     def search_library_database_by_title(self, title):
         query = "SELECT * FROM Catalog WHERE title LIKE ?"
         self.cursor.execute(query, ('%' + title + '%',))
-
-        results = []
-        for tuple in self.cursor.fetchall():
-            query = "SELECT COUNT(*) FROM Record WHERE item_id = ? AND available = True"
-            self.cursor.execute(query, (tuple[0],))
-            total = str(self.cursor.fetchone()[0]) + '/'
-            query = "SELECT COUNT(*) FROM Record WHERE item_id = ?"
-            self.cursor.execute(query, (tuple[0],))
-            total += str(self.cursor.fetchone()[0])
-            tuple = tuple + (total,)
-            results.append(tuple)
-        return results
+        return self.fetch_item_availability(self.cursor.fetchall())
 
     def search_library_database_by_id(self, id):
         query = "SELECT * FROM Catalog WHERE item_id = ?"
         self.cursor.execute(query, (id,))
-        results = []
-        for tuple in self.cursor.fetchall():
-            query = "SELECT COUNT(*) FROM Record WHERE item_id = ? AND available = True"
-            self.cursor.execute(query, (tuple[0],))
-            total = str(self.cursor.fetchone()[0]) + '/'
-            query = "SELECT COUNT(*) FROM Record WHERE item_id = ?"
-            self.cursor.execute(query, (tuple[0],))
-            total += str(self.cursor.fetchone()[0])
-            tuple = tuple + (total,)
-            results.append(tuple)
-        return results
+        return self.fetch_item_availability(self.cursor.fetchone())
 
     def search_by_author_name(self, author_name):
         query = "SELECT * FROM Catalog WHERE author_or_artist LIKE ?"
         self.cursor.execute(query, ('%' + author_name + '%',))
-        results = []
-        for tuple in self.cursor.fetchall():
-            query = "SELECT COUNT(*) FROM Record WHERE item_id = ? AND available = True"
-            self.cursor.execute(query, (tuple[0],))
-            total = str(self.cursor.fetchone()[0]) + '/'
-            query = "SELECT COUNT(*) FROM Record WHERE item_id = ?"
-            self.cursor.execute(query, (tuple[0],))
-            total += str(self.cursor.fetchone()[0])
-            tuple = tuple + (total,)
-            results.append(tuple)
-        return results
+        return self.fetch_item_availability(self.cursor.fetchall())
 
     def search_by_title_and_author_name(self, title, author_name):
         query = "SELECT * FROM Catalog WHERE author_or_artist LIKE ? AND title LIKE ?"
         self.cursor.execute(query, ('%' + author_name + '%', '%' + title + '%'))
-        results = []
-        for tuple in self.cursor.fetchall():
-            query = "SELECT COUNT(*) FROM Record WHERE item_id = ? AND available = True"
-            self.cursor.execute(query, (tuple[0],))
-            total = str(self.cursor.fetchone()[0]) + '/'
-            query = "SELECT COUNT(*) FROM Record WHERE item_id = ?"
-            self.cursor.execute(query, (tuple[0],))
-            total += str(self.cursor.fetchone()[0])
-            tuple = tuple + (total,)
-            results.append(tuple)
-        return results
+        return self.fetch_item_availability(self.cursor.fetchall())
 
     def search_by_item_type(self, item_type):
         query = "SELECT * FROM Catalog WHERE item_type = ?"
         self.cursor.execute(query, (item_type,))
-        results = []
-        for tuple in self.cursor.fetchall():
-            query = "SELECT COUNT(*) FROM Record WHERE item_id = ? AND available = True"
-            self.cursor.execute(query, (tuple[0],))
-            total = str(self.cursor.fetchone()[0]) + '/'
-            query = "SELECT COUNT(*) FROM Record WHERE item_id = ?"
-            self.cursor.execute(query, (tuple[0],))
-            total += str(self.cursor.fetchone()[0])
-            tuple = tuple + (total,)
-            results.append(tuple)
-        return results
+        return self.fetch_item_availability(self.cursor.fetchall())
 
     def search_before_target_date(self, target_date):
         query = "SELECT * FROM Catalog WHERE publication_date <= ?"
         self.cursor.execute(query, (target_date,))
-        results = []
-        for tuple in self.cursor.fetchall():
-            query = "SELECT COUNT(*) FROM Record WHERE item_id = ? AND available = True"
-            self.cursor.execute(query, (tuple[0],))
-            total = str(self.cursor.fetchone()[0]) + '/'
-            query = "SELECT COUNT(*) FROM Record WHERE item_id = ?"
-            self.cursor.execute(query, (tuple[0],))
-            total += str(self.cursor.fetchone()[0])
-            tuple = tuple + (total,)
-            results.append(tuple)
-        return results
+        return self.fetch_item_availability(self.cursor.fetchall())
 
     def search_after_target_date(self, target_date):
         query = "SELECT * FROM Catalog WHERE publication_date >= ?"
         self.cursor.execute(query, (target_date,))
+        
+    
+    def fetch_item_availability(self, items):
         results = []
-        for tuple in self.cursor.fetchall():
+        for tuple in items:
             query = "SELECT COUNT(*) FROM Record WHERE item_id = ? AND available = True"
             self.cursor.execute(query, (tuple[0],))
             total = str(self.cursor.fetchone()[0]) + '/'
